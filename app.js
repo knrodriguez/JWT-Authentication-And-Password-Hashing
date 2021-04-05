ffc3742eb6f0fc1fff6480da23b479701ed45b05
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-04-05 10:52:16 -0400

    Abstracting to Middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ app.use(express.json());
 const { models: { User, Note }} = require('./db');
 const path = require('path');
 
+const requireToken = async (req,res,next) => {
+  try {
+    const user = await User.byToken(req.headers.authorization);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
 app.post('/api/auth', async(req, res, next)=> {
@@ -15,20 +25,18 @@ app.post('/api/auth', async(req, res, next)=> {
   }
 });
 
-app.get('/api/auth', async(req, res, next)=> {
+app.get('/api/auth', requireToken, async(req, res, next)=> {
   try {
-    console.log(req.headers)
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   }
   catch(ex){
     next(ex);
   }
 });
 
-app.get('/api/users/:userId/notes', async (req,res,next) => {
+app.get('/api/users/:userId/notes', requireToken, async (req,res,next) => {
   try {
-    console.log('HEADERS', req.headers)
-    const user = await User.byToken(req.headers.authorization);
+    const user = req.user;
     if(user.id === parseInt(req.params.userId)){
       const notes = await Note.findAll({
         where: {userId: user.id}
